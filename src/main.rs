826b2d0634f7b3c5aac373cd4fcfcf6200a0bcbe
lov3b <lovebillenius@protonmail.com>
2021-12-01 21:56:57 +0100

    yeh

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,24 @@ use std::{fs, io::Read};
 fn main() {
     println!("I am alive. Now go and fuck yourself");
 
-    let countries = fs::read_to_string("countries.html")
-        .expect("There is no countries.html file");
-    for i in countries.lines(){
-        println!("NEW LINE: {}",&i);
-    }
+    let countries = fs::read_to_string("countries.html").expect("There is no countries.html file");
+    for i in countries.lines() {
+ 
+        // If we aren't on a line that contains desired link then skip
+        if !i.contains("href=") {
+            continue;
+        }
 
+        let i = i.trim().replace("<a class=\"entry-title\" href=\"", "");
+        let mut firstquote = i.find("\"").unwrap();
+        let (link, _) = i.split_at(firstquote);
+
+
+    }
+}
+
+struct Embassy<'a> {
+    country: &'a str,
+    link: &'a str,
+    mail: Option<&'a str>,
 }
