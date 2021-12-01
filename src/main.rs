3f2df6448d1d9eec2dbacc95e79e4cdedd5cb487
lov3b <lovebillenius@protonmail.com>
2021-12-01 19:20:31 +0100

    I dont know what to put here now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+//                _                                _
+//  ___ _ __ ___ | |__   __ _ ___ ___ _   _       (_)___  ___  _ __
+// / _ \ '_ ` _ \| '_ \ / _` / __/ __| | | |      | / __|/ _ \| '_ \
+//|  __/ | | | | | |_) | (_| \__ \__ \ |_| |      | \__ \ (_) | | | |
+// \___|_| |_| |_|_.__/ \__,_|___/___/\__, |____ _/ |___/\___/|_| |_|
+//                                    |___/_____|__/
+
+use std::{fs, io::Read};
 fn main() {
-    println!("Hello, world!");
+    println!("I am alive. Now go and fuck yourself");
+
+    let countries = fs::read_to_string("countries.html")
+        .expect("There is no countries.html file");
+    for i in countries.lines(){
+        println!("NEW LINE: {}",&i);
+    }
+
 }
